121f72554e44621b52145f4025571844467bc785
HP <HP@DESKTOP-915533L>
2022-10-18 22:04:38 +0530

    MiddleCharacter

---
+++ Assignment2/src/com/codebody/MiddleCharacter.java
@@ -0,0 +1,15 @@
+package com.codebody;
+import java.util.Scanner;
+public class MiddleCharacter {
+
+	public static void main(String[] args) 
+	{
+		Scanner input=new Scanner(System.in);
+		System.out.print("Enter word: ");
+		String str=input.nextLine();
+		int len=str.length();
+		int c=len/2;
+		char ch[]=str.toCharArray();
+		System.out.println("Middle Character is: "+ch[c]);
+	}
+}
